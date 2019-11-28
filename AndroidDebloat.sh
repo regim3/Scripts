8d4ab7e364b1028236bd6203d979a78d5d768e15
kushagrakarira <kushagrakarira@gmail.com>
2019-11-28 17:57:23 +0530

    Update

---
+++ AndroidDebloat.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Android Debloat
+
+echo Orignal repo. at https://github.com/KushagraKarira/Debloat
+echo Clonning remote repo.
+git clone https://github.com/KushagraKarira/Debloat
+cd Debloat/Android
+echo Print All debloat options
+#select one
+# sh execute
+
+
+echo Take time to explore my other repo. at https://github.com/KushagraKarira
